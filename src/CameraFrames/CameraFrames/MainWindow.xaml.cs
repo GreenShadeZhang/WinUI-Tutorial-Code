9e805dff4ad6a37bc4a32db043d6168180fbfe28
gil zhang <gil.zhang.dev@outlook.com>
2022-09-02 22:23:27 +0800

    添加摄像头视频帧处理代码

---
+++ src/CameraFrames/CameraFrames/MainWindow.xaml.cs
@@ -0,0 +1,169 @@
+﻿using CameraFrames;
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Automation.Peers;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Core;
+using WindowActivatedEventArgs = Microsoft.UI.Xaml.WindowActivatedEventArgs;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace SDKTemplate
+{
+    /// <summary>
+    /// An empty window that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            this.InitializeComponent();
+            SampleTitle.Text = FEATURE_NAME;
+        }
+
+        public const string FEATURE_NAME = "Camera Frames";
+
+        List<Scenario> scenarios = new List<Scenario>
+        {
+            new Scenario() { Title="Shared mode access to color, depth and infrared frame sources", ClassType=typeof(Scenario1_DisplayDepthColorIR)},
+            new Scenario() { Title="Find and display all media frame sources", ClassType=typeof(Scenario2_FindAvailableSourceGroups)},
+        };
+
+        private void Window_Activated(object sender, WindowActivatedEventArgs args)
+        {
+            // Populate the scenario list from the SampleConfiguration.cs file
+            var itemCollection = new List<Scenario>();
+            int i = 1;
+            foreach (Scenario s in scenarios)
+            {
+                itemCollection.Add(new Scenario { Title = $"{i++}) {s.Title}", ClassType = s.ClassType });
+            }
+            ScenarioControl.ItemsSource = itemCollection;
+
+            if (App.MainWindow.Bounds.Width < 640)
+            {
+                ScenarioControl.SelectedIndex = -1;
+            }
+            else
+            {
+                ScenarioControl.SelectedIndex = 0;
+            }
+        }
+        /// <summary>
+        /// Called whenever the user changes selection in the scenarios list.  This method will navigate to the respective
+        /// sample scenario page.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void ScenarioControl_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            // Clear the status block when navigating scenarios.
+            NotifyUser(String.Empty, NotifyType.StatusMessage);
+
+            ListBox scenarioListBox = sender as ListBox;
+            Scenario s = scenarioListBox.SelectedItem as Scenario;
+            if (s != null)
+            {
+                ScenarioFrame.Navigate(s.ClassType);
+                if (App.MainWindow.Bounds.Width < 640)
+                {
+                    Splitter.IsPaneOpen = false;
+                }
+            }
+        }
+
+        public List<Scenario> Scenarios
+        {
+            get { return this.scenarios; }
+        }
+
+        /// <summary>
+        /// Display a message to the user.
+        /// This method may be called from any thread.
+        /// </summary>
+        /// <param name="strMessage"></param>
+        /// <param name="type"></param>
+        public void NotifyUser(string strMessage, NotifyType type)
+        {
+            // If called from the UI thread, then update immediately.
+            // Otherwise, schedule a task on the UI thread to perform the update.
+
+            
+            if (this.DispatcherQueue.HasThreadAccess)
+            {
+                UpdateStatus(strMessage, type);
+            }
+            else
+            {
+                var task = this.DispatcherQueue.TryEnqueue(Microsoft.UI.Dispatching.DispatcherQueuePriority.Normal, () => UpdateStatus(strMessage, type));
+            }
+        }
+
+        private void UpdateStatus(string strMessage, NotifyType type)
+        {
+            switch (type)
+            {
+                case NotifyType.StatusMessage:
+                    StatusBorder.Background = new SolidColorBrush(Microsoft.UI.Colors.Green);
+                    break;
+                case NotifyType.ErrorMessage:
+                    StatusBorder.Background = new SolidColorBrush(Microsoft.UI.Colors.Red);
+                    break;
+            }
+
+            StatusBlock.Text = strMessage;
+
+            // Collapse the StatusBlock if it has no text to conserve real estate.
+            StatusBorder.Visibility = (StatusBlock.Text != String.Empty) ? Visibility.Visible : Visibility.Collapsed;
+            if (StatusBlock.Text != String.Empty)
+            {
+                StatusBorder.Visibility = Visibility.Visible;
+                StatusPanel.Visibility = Visibility.Visible;
+            }
+            else
+            {
+                StatusBorder.Visibility = Visibility.Collapsed;
+                StatusPanel.Visibility = Visibility.Collapsed;
+            }
+
+            // Raise an event if necessary to enable a screen reader to announce the status update.
+            var peer = FrameworkElementAutomationPeer.FromElement(StatusBlock);
+            if (peer != null)
+            {
+                peer.RaiseAutomationEvent(AutomationEvents.LiveRegionChanged);
+            }
+        }
+
+        async void Footer_Click(object sender, RoutedEventArgs e)
+        {
+            await Windows.System.Launcher.LaunchUriAsync(new Uri(((HyperlinkButton)sender).Tag.ToString()));
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Splitter.IsPaneOpen = !Splitter.IsPaneOpen;
+        }
+    }
+    public enum NotifyType
+    {
+        StatusMessage,
+        ErrorMessage
+    };
+    public class Scenario
+    {
+        public string Title { get; set; }
+        public Type ClassType { get; set; }
+    }
+}
---
+++ src/CameraFrames/CameraFrames/SimpleLogger.cs
@@ -0,0 +1,57 @@
+﻿//*********************************************************
+//
+// Copyright (c) Microsoft. All rights reserved.
+// This code is licensed under the Microsoft Public License.
+// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
+// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
+// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
+// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
+//
+//*********************************************************
+
+using CameraFrames;
+using Microsoft.UI.Xaml.Controls;
+using System;
+using Windows.UI.Core;
+
+namespace SDKTemplate
+{
+    /// <summary>
+    /// A simple logger to display text to TextBlock asynchronisely.
+    /// </summary>
+    class SimpleLogger
+    {
+        private CoreDispatcher _dispatcher;
+        private TextBlock _textBlock;
+        private string _messageText = string.Empty;
+        private readonly object _messageLock = new object();
+        private int _messageCount;
+
+        public SimpleLogger(TextBlock textBlock)
+        {
+            _textBlock = textBlock;
+            _dispatcher = _textBlock.Dispatcher;
+        }
+
+        /// <summary>
+        /// Logs a message to be displayed.
+        /// </summary>
+        internal async void Log(string message)
+        {
+            var newMessage = $"[{_messageCount++}] {DateTime.Now.ToString("hh:MM:ss")} : {message}\n{_messageText}";
+
+            lock (_messageLock)
+            {
+                _messageText = newMessage;
+            }
+
+            App.MainWindow.DispatcherQueue.TryEnqueue(Microsoft.UI.Dispatching.DispatcherQueuePriority.Low, () =>
+            {
+                lock (_messageLock)
+                {
+                    _textBlock.Text = _messageText;
+                }
+            });
+        }
+    }
+}
